85d0ab0f8bcf6543605680878c394071f13c54d7
Samantha Perry <samnperry@arizona.edu>
2024-06-19 10:30:54 +0300

    Updated comments and added package-info for Anti-patterns

--- src/main/java/edu/university/ecs/lab/detection/antipatterns/models/WrongCuts.java
+++ src/main/java/edu/university/ecs/lab/detection/antipatterns/models/WrongCuts.java
@@ -5,10 +5,21 @@ import lombok.Data;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * Represents a cluster of wrongly interconnected services (Wrong Cuts) detected in a microservice network graph.
+ */
 @Data
 public class WrongCuts {
+    /**
+     * Set of service names forming a cluster of wrongly interconnected services.
+     */
     private Set<String> wrongCuts = new HashSet<>();
 
+    /**
+     * Constructs a WrongCuts object initialized with the provided set of wrongly interconnected service names.
+     *
+     * @param wrongCuts Set of service names forming a cluster of wrongly interconnected services.
+     */
     public WrongCuts(Set<String> wrongCuts) {
         this.wrongCuts = wrongCuts;
     }
