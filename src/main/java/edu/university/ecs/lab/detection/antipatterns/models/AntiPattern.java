6df5bdd98bb9f51501b8750155589558cadd5957
Gabriel Goulis <ninjagabe2003@gmail.com>
2024-06-19 11:40:33 +0300

    Antipattern abstract class template

---
+++ src/main/java/edu/university/ecs/lab/detection/antipatterns/models/AntiPattern.java
@@ -0,0 +1,27 @@
+package edu.university.ecs.lab.detection.antipatterns.models;
+
+import com.google.gson.JsonObject;
+import edu.university.ecs.lab.common.models.serialization.JsonSerializable;
+
+import java.io.Serializable;
+
+/**
+ * Abstract implementation of an Antipattern should be the parent
+ * of all system Antipatterns
+ */
+public abstract class AntiPattern implements JsonSerializable {
+    protected abstract String getName();
+    protected abstract String getDescription();
+    protected abstract JsonObject getMetaData();
+
+    @Override
+    public final JsonObject toJsonObject() {
+        JsonObject jsonObject = new JsonObject();
+
+        jsonObject.addProperty("name", getName());
+        jsonObject.addProperty("description", getDescription());
+        jsonObject.add("metaData", getMetaData());
+
+        return jsonObject;
+    }
+}
