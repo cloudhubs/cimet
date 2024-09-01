521a0afce07e5da7cc2ca800ff8bad661bf8faed
Gabriel Goulis <ninjagabe2003@gmail.com>
2024-09-01 08:12:07 -0500

    Workflow for maven testing, all tests passing

--- src/test/java/integration/Constants.java
+++ src/test/java/integration/Constants.java
@@ -1,7 +1,7 @@
 package integration;
 
 public class Constants {
-    public static final String TEST_CONFIG_PATH = "C:\\Users\\ninja\\IdeaProjects\\cimet2\\src\\test\\resources\\test_config.json";
+    public static final String TEST_CONFIG_PATH = "./src/test/resources/test_config.json";
     public static final String OLD_IR_PATH = "./output/OldIR.json";
     public static final String NEW_IR_PATH = "./output/NewIR.json";
     public static final String TEST_IR_PATH = "./output/TestIR.json";
